43c35f71a2f415286d3714edf9b39c2ad08535b3
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-27 10:11:18 +0900

    0526_CodingTest_주석추가

--- Coding_Test/0526/3/CodingTest03_주익수.java
+++ Coding_Test/0526/3/CodingTest03_주익수.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int N) {
-        int[] dp = new int[11];
+        int[] dp = new int[11]; //dynamicProgramming으로 판단하였음
 
-        dp[0] = 1;
-        dp[1] = 2;
+        dp[0] = 1; //첫번째는 1
+        dp[1] = 2; //두번째는 2
 
-        for (int i = 2; i <= 10; i++) {
+        for (int i = 2; i <= 10; i++) { //다음과 같은 점화식사용
             dp[i] = dp[i-1] + dp[i-2];
         }
 
