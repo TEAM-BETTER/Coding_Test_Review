c2c978b762908319d9f9b83c3ce4f01b1813de48
Guiwoo <park.guiwoo@hotmail.com>
2022-05-27 10:50:58 +0900

    문제 2번 문제 4번 주석 수정

--- Coding_Test/0526/4/codingTest[04]_[박귀우].java
+++ Coding_Test/0526/4/codingTest[04]_[박귀우].java
@@ -17,7 +17,7 @@ class Solution {
      public int checkWhere(int size,int start,int end,int i,int k){
          //각각 사이즈는 배열의 사이즈 즉 size*size 를  의미
          // start,end 는 각각 그 배열의 시작점 과 끝을 의미
-         // i,k 는 원하고자 하는 좌표
+         // i,k 얻고자 하는 좌표
         if(size == 2){ // 사이즈가 2라면 총 [2][2] 사이지의 배열 일경우 아래와 같이 처리
             int[][] answer = new int[2][2];
             answer[0][1] = end-3; 
@@ -39,6 +39,7 @@ class Solution {
          * 11 12|15 16
          */
         int nextEnd= (end-start+1)/4; // 사분면의 정확한 지점이 확정될시 사이즈를 줄이고 시작점과 끝점 변경
+        // 시작점 끝점 그리고 각 좌표를 변경 시켜줘야함 사이즈의 크기를 줄여나갈거기 떄문에
         if(top && right) {//1사 분면 "시작점 동일 끝점 은 시작점에 4분면의 끝점-1" -1인이유는 1부터 시작하기떄문에
             return checkWhere(size/2,start,start+nextEnd-1,i,k-(size/2));
         }else if(top && left){//2사분 면 "시작점 은 사분면 의 첫번째 시작점을 더해준값"
