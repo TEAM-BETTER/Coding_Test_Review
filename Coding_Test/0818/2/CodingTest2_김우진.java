92a11b4cd5b470367e86ce7d245ccd7c582e3bd3
AMYY_ZZIN <101795447+amyyzzin@users.noreply.github.com>
2022-08-20 19:27:19 +0900

    Update CodingTest2_김우진.java

--- Coding_Test/0818/2/CodingTest2_김우진.java
+++ Coding_Test/0818/2/CodingTest2_김우진.java
@@ -1,50 +1,67 @@
 package CodingTest13;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * 정확성 : 10점 / 효율성 : 4점 (시간 초과(2,5) 및 메모리 초과(4))
- *      3중포문으로 풀었기 때문에 시간 및 메모리 초과가 날 것이라 예상했습니다.
- *      투포인터로 풀어볼까 했는데 런타임에러로 10점 나왔고 최종 정답처리는 아래 답안이 되었습니다
- *      주말에 다시 풀어보고 답안 올리겠습니다
+ * 1. 초기 숫자 3개 : arr[i], arr[i+1], arr[arr.length-1]
+ *      arr[i]는 고정으로 두고 (i + 1)과 (arr.length - 1)가 투 포인터
  *
- * 1. 3개의 수를 3중 포문으로 돌려 나온 값은 ArrayList에 넣음
- *      여기서 target과 같은 값 발견 시, 리턴
+ * 2. haveToUpdate 메서드 : 현재 sum을 answer로 업데이트할지 여부를 판단하는 메서드
  *
- * 2. ArrayList의 숫자들과 target의 차이값 구한 후
- *      가장 작은 차이값을 더하거나 뺀 숫자 중에
- *      ArrayList에 있는 값으로 리턴
+ * 3. 투포인터로 target값을 탐색, 찾으면 반복 종료,
+ *      현재 sum이 target 값보다 작을 경우 sum을 증가시켜야하므로 left++
+ *      현재 sum이 target 값보다 클 경우 sum을 감소시켜야하므로 right--
+ *      현재 sum이 target이면 답이므로 바로 target 반환
+
+ ** 없으면 시간초과1 : arr[i] == arr[i - 1]
+ *      어차피 같은 값이기 때문에 계산하지말고 다음으로 넘어가는게 시간 줄이기
+ *
+ *** 없으면 시간초과2: target값을 찾으면 더 이상 탐색할 필요가 없음
  *
  */
 
-public class CodingTest2_김우진 {
+public class CodingTest2 {
+    static boolean haveToUpdate(int answer, int sum, int target) {
+        if (Math.abs(target - answer) == Math.abs(target - sum)) {
+            return answer > sum;
+        }
+
+        return Math.abs(target - answer) > Math.abs(sum - target);
+    }
 
     public static int solution(int[] arr, int target) {
-        int answer = 0;
-        ArrayList<Integer> arrClone = new ArrayList<>();
         Arrays.sort(arr);
-        int abs = Integer.MAX_VALUE;
+        int answer = 100000000 + 30001;
 
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                for (int k = j + 1; k < arr.length; k++) {
-                    int sum = arr[i] + arr[j] + arr[k];
-                    arrClone.add(sum);
-                    abs = Math.min(Math.abs(sum - target), abs);
+            int left = i + 1;
+            int right = arr.length - 1;
 
-                    if (sum == target) {
-                        return sum;
-                    }
+            //없으면 시간초과 1
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+
+                if (haveToUpdate(answer, sum, target)) {
+                    answer = sum;
+                }
+
+                //없으면 시간초과 2
+                if (sum == target) {
+                    return target;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
 
-        if (arrClone.contains(target - abs)) {
-            answer = target - abs;
-        } else if (arrClone.contains(target + abs)) {
-            answer = target + abs;
-        }
         return answer;
     }
 
